7e5704a7effe8afaf30a4012caf892b487b90ae5
Pauline Urkia <urkia.p@free.fr>
2022-03-23 23:09:14 +0100

    create button cmp

---
+++ src/components/MainButton.vue
@@ -0,0 +1,10 @@
+<template>
+	<div
+		class="text-white w-8/12 border-white border-dashed border-2 mx-auto p-2 text-center rounded-xl hover:bg-white hover:text-main-grey hover:border-main-grey"
+	>
+		<slot></slot>
+	</div>
+</template>
+<script setup lang="ts">
+defineProps<{ msg: string }>();
+</script>
