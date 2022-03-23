8af3b57984f811a723d81cfa9a17923ababf76a5
Pauline Urkia <urkia.p@free.fr>
2022-03-23 22:06:09 +0100

    remove default template

---
+++ src/components/MainPresenter.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="bg-grey-bg-500">test</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
 	<main>
-		<TheWelcome />
+		<MainPresenter />
 	</main>
 </template>
 
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import MainPresenter from "@/components/MainPresenter.vue";
 </script>
