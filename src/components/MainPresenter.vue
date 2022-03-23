ccaf56288ab986061a8bb93ed3fd8cdd922f8087
Pauline Urkia <urkia.p@free.fr>
2022-03-23 23:16:56 +0100

    add blink animation

--- src/components/MainPresenter.vue
+++ src/components/MainPresenter.vue
@@ -1,5 +1,7 @@
 <template>
-	<div class="bg-main-grey h-3/12 w-4/12 space-y-6 flex flex-col">
+	<div class="bg-main-grey h-3/12 w-4/12 space-y-6 flex flex-col p-4">
+		<BlinkAnimation class="w-96 mx-auto" image-name="logo hd menu/logo" />
+
 		<Button :msg="'Jouer sur navigateur'"> Jouer sur navigateur </Button>
 		<Button :msg="'Jouer sur navigateur'"> Télécharger </Button>
 		<Button :msg="'Jouer sur navigateur'"> Jouer sur android </Button>
@@ -7,4 +9,5 @@
 </template>
 <script setup lang="ts">
 import Button from "./MainButton.vue";
+import BlinkAnimation from "./BlinkAnimation.vue";
 </script>
