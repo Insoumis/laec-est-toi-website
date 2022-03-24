a09bae9ae0ddbb1a58f9ec4fdc4ef6c30a503534
Pauline Urkia <urkia.p@free.fr>
2022-03-24 23:02:41 +0100

    styling button

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="text-white w-8/12 border-white mx-auto p-2 text-center rounded-xl hover:text-gold"
+		class="text-white text-2xl w-8/12 border-white mx-auto p-2 text-center rounded-xl hover:text-gold"
 	>
 		<slot></slot>
 	</div>
