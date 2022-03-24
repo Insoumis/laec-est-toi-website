620d3dbb08b4bd3840e8616efb662469d6fe4c7a
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2022-03-24 19:00:02 +0100

    Add BackgroundImages component

---
+++ src/components/BackgroundImages.vue
@@ -0,0 +1,29 @@
+<template>
+	<img
+		class="fixed top-0 object-cover w-full h-full"
+		:src="gradientUrl"
+		alt="gradient de fond"
+	/>
+	<img
+		class="fixed bottom-0 object-cover w-full h-full"
+		:src="blueOverlayUrl"
+		alt="éoliennes bleues"
+	/>
+	<img
+		class="fixed bottom-0 object-cover w-full h-full"
+		:src="yellowOverlayUrl"
+		alt="fusées jaunes"
+	/>
+</template>
+
+<script setup lang="ts">
+const gradientUrl = new URL(`../assets/calques/0_gradient.png`, import.meta.url)
+	.href;
+
+const blueOverlayUrl = new URL(`../assets/calques/1_bleu.png`, import.meta.url)
+	.href;
+const yellowOverlayUrl = new URL(
+	`../assets/calques/2_jaune.png`,
+	import.meta.url
+).href;
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,13 @@
 <template>
 	<main
-		:style="{
-			backgroundImage: `url('${gradientUrl}')`,
-		}"
-		class="h-screen relative w-full flex justify-center align-center p-16"
+		class="relative flex justify-center w-full min-h-screen p-16 align-center"
 	>
-		<img
-			class="absolute left-0 bottom-0"
-			:src="blueOverlayUrl"
-			alt="éoliennes bleues"
-		/>
-		<img
-			class="absolute right-0 bottom-0"
-			:src="yellowOverlayUrl"
-			alt="fusées jaunes"
-		/>
-
+		<BackgroundImages />
 		<MainPresenter class="relative" />
 	</main>
 </template>
 
 <script setup lang="ts">
+import BackgroundImages from "@/components/BackgroundImages.vue";
 import MainPresenter from "@/components/MainPresenter.vue";
-
-const gradientUrl = new URL(`../assets/calques/0_gradient.png`, import.meta.url)
-	.href;
-
-const blueOverlayUrl = new URL(`../assets/calques/1_bleu.png`, import.meta.url)
-	.href;
-const yellowOverlayUrl = new URL(
-	`../assets/calques/2_jaune.png`,
-	import.meta.url
-).href;
 </script>
