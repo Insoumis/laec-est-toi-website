a6acadf249b9d91abfd8f8a55c7d4e7f6143263d
Pauline Urkia <urkia.p@free.fr>
2022-03-25 22:15:13 +0100

    add 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import PageNotFound from "../views/PageNotFound.vue";
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
@@ -13,6 +13,11 @@ const router = createRouter({
 			path: "/game",
 			name: "game",
 			component: () => import("../views/GameView.vue"),
+		},
+		{
+			path: "/:pathMatch(.*)*",
+			name: "404",
+			component: PageNotFound,
 		},
 	],
 });
