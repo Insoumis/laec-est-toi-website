9ea9b684459426393fbb2c8dcc195b785da187e7
Nathanaël Labreuil <nathanael.labreuil@gmail.com>
2022-03-25 20:04:07 +0100

    Remove font size variation on buttons

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="text-white md:text-3xl text-2xl border-white p-2 text-center rounded-xl hover:text-gold pointer"
+		class="text-white text-3xl border-white p-2 text-center rounded-xl hover:text-gold pointer"
 	>
 		<slot></slot>
 	</div>
