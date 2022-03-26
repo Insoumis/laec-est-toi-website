d30a2bcc498f4112d26629722e06179c1a5a58d1
Pauline Urkia <urkia.p@free.fr>
2022-03-26 14:57:12 +0100

    fix typo

--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -7,27 +7,27 @@
 		class="relative flex justify-center my-auto w-full sm:p-16 align-center"
 	>
 		<div
-			class="flex font-sofia relative h- flex-col sm:w-4/12 w-screen md:px-16 md:py-12 p-8 border border-black shadow-2xl bg-main-grey sm:rounded-2xl sm:min-w-[450px] text-white"
+			class="flex font-sofia relative flex-col sm:w-5/12 w-screen md:px-16 md:py-12 p-8 border border-black shadow-2xl bg-main-grey sm:rounded-2xl sm:min-w-[450px] text-white"
 		>
 			<div class="text-6xl font-draxel text-center">Remerciements</div>
-			<div class="text-center space-y-4 mt-6 flex flex-col">
-				<div class="italic font-sofia">
-					D'après une idée de Trollune et Fanélia
+			<div class="text-center text-2xl space-y-4 mt-6 flex flex-col">
+				<div class="italic font-sofia m-4">
+					D'après une idee de Trollune et Fanelia
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">DEVELOPPEURS</div>
+					<div class="text-3xl mt-2">DEVELOPPEURS</div>
 					<div>Adrenesis</div>
 					<div>Trollune</div>
 					<div>Roipoussiere</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">DEVELOPPEURS D'OUTILS</div>
+					<div class="text-3xl mt-2">DEVELOPPEURS D'OUTILS</div>
 
 					<div>Adrenesis</div>
 					<div>Trollune</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">GRAPHISTES</div>
+					<div class="text-3xl mt-2">GRAPHISTES</div>
 
 					<div>Lametyste</div>
 					<div>Niwatori</div>
@@ -40,29 +40,29 @@
 					<div>Manda G</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">ARTISTES TECHNIQUE</div>
+					<div class="text-3xl mt-2">ARTISTES TECHNIQUE</div>
 
 					<div>Adrenesis</div>
 					<div>Trollune</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">COMPOSITION MUSICALE</div>
+					<div class="text-3xl mt-2">COMPOSITION MUSICALE</div>
 
 					<div>Precheurius Studio CK Project</div>
 					<div>AlphaXIII Studio CK Project</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">PERFORMANCE MUSICALE</div>
+					<div class="text-3xl mt-2">PERFORMANCE MUSICALE</div>
 
 					<div>Precheurius Studio CK Project</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">BRUITAGES</div>
+					<div class="text-3xl mt-2">BRUITAGES</div>
 
 					<div>Wilfried</div>
 				</div>
 				<div class="font-draxel">
-					<div class="text-xl mt-2">CONCEPTS DE JEU</div>
+					<div class="text-3xl mt-2">CONCEPTS DE JEU</div>
 
 					<div>Trollune</div>
 					<div>Cyril M.</div>
@@ -71,7 +71,8 @@
 					<div>IvanC</div>
 				</div>
 				<div class="font-draxel">
-					CONCEPTION DE NIVEAU
+					<div class="text-3xl mt-2">CONCEPTION DE NIVEAU</div>
+
 					<div>Miidnight</div>
 					<div>Trollune</div>
 					<div>Dazo</div>
@@ -84,19 +85,22 @@
 				</div>
 
 				<div class="font-draxel">
-					ORGANISATION
+					<div class="text-3xl mt-2">ORGANISATION</div>
+
 					<div>Miidnight</div>
 					<div>Precheurius Studio CK Project</div>
 				</div>
 
 				<div class="font-draxel">
-					WEBSITE
+					<div class="text-3xl mt-2">WEBSITE</div>
+
 					<div>Aikru</div>
 					<div>Nate Lafeuille</div>
 				</div>
 
 				<div class="font-draxel">
-					TRAILER
+					<div class="text-3xl mt-2">TRAILER</div>
+
 					<div>Zegueux</div>
 					<div>Precheurius Studio CK Project</div>
 					<div>Ista</div>
@@ -105,22 +109,25 @@
 				</div>
 
 				<div class="font-draxel">
-					VERIFICATION
+					<div class="text-3xl mt-2">VERIFICATION</div>
+
 					<div>Koten</div>
 				</div>
 				<div class="font-draxel">
-					TESTEUR⋅ES
+					<div class="text-3xl mt-2">TESTEUR⋅ES</div>
+
 					<div>DanLo</div>
 					<div>NightGlow</div>
 					<div>JeanSebastienBachOfficiel</div>
 					<div>SpyBob</div>
 					<div>anthony</div>
-					<div>Mattéo & Gianni</div>
+					<div>Matteo & Gianni</div>
 					<div>Saccharose4965</div>
 				</div>
 
 				<div class="font-draxel">
-					COMMUNICATION
+					<div class="text-3xl mt-2">COMMUNICATION</div>
+
 					<div>Dazo</div>
 					<div>Miidnight</div>
 					<div>Da_Schtroumpf</div>
@@ -129,7 +136,8 @@
 				</div>
 
 				<div class="font-draxel">
-					REMERCIEMENTS
+					<div class="text-3xl mt-2">REMERCIEMENTS</div>
+
 					<div>Delphou</div>
 					<div>Fanélia</div>
 					<div>Karibou</div>
@@ -142,7 +150,8 @@
 					<div>Sno</div>
 				</div>
 				<div class="font-draxel">
-					REMERCIEMENTS SPÉCIAUX
+					<div class="text-3xl mt-2">REMERCIEMENTS SPECIAUX</div>
+
 					<div>BABA IS YOU</div>
 					<div>Kenney</div>
 					<div>Le Bon Sens</div>
