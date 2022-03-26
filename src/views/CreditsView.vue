2064d36c318b815bfd75c67baf0f286bb796d417
Pauline Urkia <urkia.p@free.fr>
2022-03-26 14:45:37 +0100

    add  credits

--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -9,84 +9,146 @@
 		<div
 			class="flex font-sofia relative h- flex-col sm:w-4/12 w-screen md:px-16 md:py-12 p-8 border border-black shadow-2xl bg-main-grey sm:rounded-2xl sm:min-w-[450px] text-white"
 		>
-			<div class="text-5xl font-draxel text-center">Remerciements</div>
-			<div class="text-center space-y-2 mt-6">
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+			<div class="text-6xl font-draxel text-center">Remerciements</div>
+			<div class="text-center space-y-4 mt-6 flex flex-col">
+				<div class="italic font-sofia">
+					D'après une idée de Trollune et Fanélia
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">DEVELOPPEURS</div>
+					<div>Adrenesis</div>
+					<div>Trollune</div>
+					<div>Roipoussiere</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">DEVELOPPEURS D'OUTILS</div>
+
+					<div>Adrenesis</div>
+					<div>Trollune</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">GRAPHISTES</div>
+
+					<div>Lametyste</div>
+					<div>Niwatori</div>
+					<div>AudreyH</div>
+					<div>Koten</div>
+					<div>Trollune</div>
+					<div>Zegueux</div>
+					<div>Da_Schtroumpf</div>
+					<div>Miidnight</div>
+					<div>Manda G</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">ARTISTES TECHNIQUE</div>
+
+					<div>Adrenesis</div>
+					<div>Trollune</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">COMPOSITION MUSICALE</div>
+
+					<div>Precheurius Studio CK Project</div>
+					<div>AlphaXIII Studio CK Project</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">PERFORMANCE MUSICALE</div>
+
+					<div>Precheurius Studio CK Project</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">BRUITAGES</div>
+
+					<div>Wilfried</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					<div class="text-xl mt-2">CONCEPTS DE JEU</div>
+
+					<div>Trollune</div>
+					<div>Cyril M.</div>
+					<div>Roipoussiere</div>
+					<div>Stheal</div>
+					<div>IvanC</div>
 				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
-				</div>
-				<div>
-					Lorem ipsum <span class="italic"> - dolores ombrage</span>
+				<div class="font-draxel">
+					CONCEPTION DE NIVEAU
+					<div>Miidnight</div>
+					<div>Trollune</div>
+					<div>Dazo</div>
+					<div>IvanC</div>
+					<div>Zargett</div>
+					<div>Copeau</div>
+					<div>Koten</div>
+					<div>anthony</div>
+					<div>Stheal</div>
 				</div>
 
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-					Voluptas natus voluptatem facere, modi, corporis quas
-					placeat quo quam dolore labore obcaecati ducimus porro
-					tempora autem ex sint exercitationem nesciunt eum.
-				</p>
+				<div class="font-draxel">
+					ORGANISATION
+					<div>Miidnight</div>
+					<div>Precheurius Studio CK Project</div>
+				</div>
+
+				<div class="font-draxel">
+					WEBSITE
+					<div>Aikru</div>
+					<div>Nate Lafeuille</div>
+				</div>
+
+				<div class="font-draxel">
+					TRAILER
+					<div>Zegueux</div>
+					<div>Precheurius Studio CK Project</div>
+					<div>Ista</div>
+					<div>Trollune</div>
+					<div>Dazo</div>
+				</div>
+
+				<div class="font-draxel">
+					VERIFICATION
+					<div>Koten</div>
+				</div>
+				<div class="font-draxel">
+					TESTEUR⋅ES
+					<div>DanLo</div>
+					<div>NightGlow</div>
+					<div>JeanSebastienBachOfficiel</div>
+					<div>SpyBob</div>
+					<div>anthony</div>
+					<div>Mattéo & Gianni</div>
+					<div>Saccharose4965</div>
+				</div>
+
+				<div class="font-draxel">
+					COMMUNICATION
+					<div>Dazo</div>
+					<div>Miidnight</div>
+					<div>Da_Schtroumpf</div>
+					<div>Zegueux</div>
+					<div>Majhinell</div>
+				</div>
+
+				<div class="font-draxel">
+					REMERCIEMENTS
+					<div>Delphou</div>
+					<div>Fanélia</div>
+					<div>Karibou</div>
+					<div>Ista</div>
+					<div>NightGlow</div>
+					<div>Gobz</div>
+					<div>Cyril M.</div>
+					<div>QGuLL</div>
+					<div>Wesson</div>
+					<div>Sno</div>
+				</div>
+				<div class="font-draxel">
+					REMERCIEMENTS SPÉCIAUX
+					<div>BABA IS YOU</div>
+					<div>Kenney</div>
+					<div>Le Bon Sens</div>
+					<div>Cyril M.</div>
+					<div>Herastodes</div>
+				</div>
 			</div>
 		</div>
 	</div>
